5b4cbadcb0dc8c23914d2b1d63fba90f12905ef1
Divy Srivastava <dj.srivastava23@gmail.com>
2019-04-23 13:45:25 +0530

    add fb-oauth - incomplete

--- config/oauth.js
+++ config/oauth.js
@@ -4,7 +4,7 @@ var google_client_id = "<CLIENT_ID>";
     google_auth_url = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=' + google_redirect_url + '&scope=profile%20email&client_id=' + google_client_id
     fb_client_id = "<client_secret>";
     fb_client_secret = "<client_secret>";
-    fb_redirect_url = "<host>:<port>/authorize/facebook";
+    fb_redirect_url = "<host>:<port>/authorize/auth/facebook/callback";
     fb_auth_url = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=' + fb_redirect_url + '&scope=profile%20email&client_id=' + fb_client_id
 
 module.exports = {
