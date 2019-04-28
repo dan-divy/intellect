4e2fe50d0d8dfa637d9aed5b9498a730364dc8e1
Divy Srivastava <dj.srivastava23@gmail.com>
2019-04-28 19:22:49 +0530

    Change Oauth config for automatic-script-friendly interface

--- config/oauth.js
+++ config/oauth.js
@@ -1,13 +1,13 @@
-var google_client_id = "<CLIENT_ID>";
-    google_client_secret = "<CLIENT_SECRET>";
+var google_client_id = "<GOOGLE_CLIENT_ID>";
+    google_client_secret = "<GOOGLE_CLIENT_SECRET>";
     google_redirect_url = "<host>:<port>/authorize/auth/google/callback";
     google_auth_url = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=' + google_redirect_url + '&scope=profile%20email&client_id=' + google_client_id
-    fb_client_id = "<CLIENT_ID>";
-    fb_client_secret = "<CLIENT_SECRET>";
+    fb_client_id = "<FB_CLIENT_ID>";
+    fb_client_secret = "<FB_CLIENT_SECRET>";
     fb_redirect_url = "<host>:<port>/authorize/auth/facebook/callback";
     fb_auth_url = 'https://www.facebook.com/v3.2/dialog/oauth?client_id='+fb_client_id+'&redirect_uri='+fb_redirect_url+'&state="{st=state123abc,ds=123456789}"'
-    twitter_api_key = "<api_key>"
-    twitter_api_secret = "<api_secret>";
+    twitter_api_key = "<TWITTER_API_KEY>"
+    twitter_api_secret = "<TWITTER_API_SECRET>";
     twitter_redirect_url = "<host>:<port>/authorize/auth/twitter/callback"
 module.exports = {
           "google": {
