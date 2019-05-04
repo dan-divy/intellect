3c4b818bba84884a6a88e2495b5efb831aea5838
MayorChano <Dannyrwilson@me.com>
2019-05-04 15:26:50 +0200

    Updatessss

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,24 @@ var array_tools = require("array-tools");
 /** Other important utilities **/
 var user = require('../utils/handlers/user');
 /** NewsAPI for real-time news feeds **/
+const Question = require('../utils/models/question');
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('a4c4e845fea64f9e9c72541aa354a29e').v2;
 
 /** Home page. **/
 router.get('/',isLoggedIn, function(req, res, next) {
   // Once user has logged in, show the home page.
-    res.render('index', {user:{name:req.session.user}})
+  Question
+    .find({})
+    .exec(async (err, obj) => {
+        await obj.forEach(q => {
+            q.timeago = ta.ago(q.date);
+        })
+      res.render('index',{
+        questions:obj,
+        user:{name:req.session.user}
+      });
+});
 });
 
 /**
