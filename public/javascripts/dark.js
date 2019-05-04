7a9dbced8368639d443cc48143f951f6937eadd3
Divy Srivastava <dj.srivastava23@gmail.com>
2019-05-04 15:31:58 +0530

    Better dark mode

--- public/javascripts/dark.js
+++ public/javascripts/dark.js
@@ -2,11 +2,13 @@ window.toggleDarkMode = function () {
   $("*").addClass("border-dark");
   $('body').removeClass()
   $("body").addClass('dark-mode');
+  $("a,p,h1,h2,h3,h4,h5").addClass('light')
   localStorage.setItem("theme","dark");
 }
 
 window.toggleLightMode = function () {
   $("*").removeClass("border-dark");
+  $("a,p,h1,h2,h3,h4,h5").removeClass('light')
   $('body').removeClass()
   $("body").addClass('light-mode');
   localStorage.setItem("theme","light");
