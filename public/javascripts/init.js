9787086baecfa6e2384c507625b79ce5a34ff902
Divy Srivastava <dj.srivastava23@gmail.com>
2019-05-12 21:53:01 +0530

    something

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -10,6 +10,30 @@ window.addEventListener('load',function () {
         break;
       //default: window.toggleLightMode();
     }
+	
+$.ajax({
+  headers: {
+    Accept: "text/plain; charset=utf-8",
+    "Content-Type": "text/plain; charset=utf-8"
+  },
+  method: 'GET',
+  url:"https://icanhazdadjoke.com/slack",
+  success : function(response) {
+    document.getElementById("quote").innerHTML = `<a style="color:black;" href="${response.attachments[0].footer.split('<')[1].split('|')[0]}">"${response.attachments[0].text}"</a>`;
+        console.log(response)
+    //setTimeout(function () {
+     //$('.box').fadeOut('slow');
+      $('.box')[0].style.display == 'none';
+   // }, 2000); // Time taken to quickly read the joke.
+  },
+  error: function (response) {
+    $("#quote").text('"Maturity is when you understand that you are imature."');
+    //setTimeout(function () {
+      //$('.box').fadeOut('slow');
+      $('.box')[0].style.display == 'none';
+    //}, 2000); // Time taken to quickly read the joke.
+}
+});
 
     Intellect.init()
 
@@ -30,36 +54,13 @@ window.changeTheme = function () {
   }
 }
 window.oldBrowser = false;
-$.ajax({
-  headers: {
-    Accept: "text/plain; charset=utf-8",
-    "Content-Type": "text/plain; charset=utf-8"
-  },
-  method: 'GET',
-  url:"https://icanhazdadjoke.com/slack",
-  success : function(response) {
-    document.getElementById("quote").innerHTML = `<a style="color:black;" href="${response.attachments[0].footer.split('<')[1].split('|')[0]}">"${response.attachments[0].text}"</a>`;
-        console.log(response)
-    //setTimeout(function () {
-     $('.box').fadeOut('slow');
-      window.checkLoadOut();
-   // }, 2000); // Time taken to quickly read the joke.
-  },
-  error: function (response) {
-    $("#quote").text('"Maturity is when you understand that you are imature."');
-    //setTimeout(function () {
-      $('.box').fadeOut('slow');
-      window.checkLoadOut();
-    //}, 2000); // Time taken to quickly read the joke.
-}
-});
-
 window.checkLoadOut = function() {
    if($('.box')[0].style.display !== 'none') {
      window.oldBrowser = true;
       $('.box')[0].style.display == 'none';
     }
 }
+//alert('hi')
 function openBar() {
   if($('#search-bar').hasClass('show-bar') && $('#search-bar').val().length > 0) {
     return window.location.href = '/search?q=' + $('#search-bar').val()
