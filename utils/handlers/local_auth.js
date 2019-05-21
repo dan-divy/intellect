7c7dfb1f067b367139ed496d469b4204ca0f8969
Divy Srivastava <dj.srivastava23@gmail.com>
2019-05-21 11:23:09 +0530

    incomplete - Update local auth

--- utils/handlers/local_auth.js
+++ utils/handlers/local_auth.js
@@ -1,37 +1,39 @@
 var User = require('../models/user');
+var bcrypt = require('bcrypt-nodejs');
 /***
 NOTE:
 
 FOR TESTING ONLY!!!!!!!
 
 ***/
-class Authenticate {
-  constructor(request) {
-
-  }
-
-  create(cb) {
+class Register {
+  constructor(request, cb) {
     // SIGN UP CODE HERE
+    this.username = request.body.username;
+    this.password = request.body.password;
+    this.firstname = request.body.firstname;
+    this.lastname = request.body.lastname;
     User
     .findOne({
       'profile.username':this.username,
       'profile.service':'local'
     }, (err, profile) => {
       if(err) return cb(err)
-      var newUser = new User({
-        profile:{
-          username:this.username,
-          password:newUser.generateHash(this.password),
-          firstname:this.firstname,
-          lastname:this.lastname
-        }
-      })
+      var newUser = new User()
+      newUser.profile = {
+        username:this.username,
+        firstname:this.firstname,
+        lastname:this.lastname,
+        password: newUser.generateHash(this.password),
+        service:'local'
+      }
       newUser.save((error, done) => {
         return cb(null, done)
       })
 
     })
   }
+
 }
 class Login {
   constructor(request, cb) {
@@ -43,45 +45,31 @@ class Login {
       'profile.username':this.username,
       'profile.service':'local'
     },(err, profile) => {
-      console.log('asd')
       if(err) return cb(err)
-      return cb(null, profile)
-    })
+      console.log(bcrypt.compareSync(this.password, profile.password))
+      bcrypt.compare(this.password, profile.password,(error ,matches) => {
+        if(matches) {
+          return cb(null, profile.profile);
+        }
+        else {
+          return cb(null, false);
+        }
+      })
+     })
   }
 
 }
 
-function login(request, cb) {
-  // LOGIN CODE HERE
-  this.username = request.body.username;
-  this.password = request.body.password;
-  User
-  .find({
-    'profile.username':this.username,
-    'profile.service':'local'
-  })
-  .exec((err, profile) => {
-    console.log('asd')
-    if(err) return cb(err)
-    return cb(null, profile)
-  })
-}
-/**
-login({
+
+new Login({
   body: {
-    username:"Divy",
-    password:"123"
+    username:"undefined_void",
+    password:"123",
+    firstname:"S",
+    lastname:"S"
   }
-}, (err, done)=> {
+}, (err, done) => {
+  if(err) throw console.error(err);
+  //if(!done) res.render('auth/login')
   console.log(done)
-}) **/
-console.log('Starting')
-User
-.findOne({
-  'profile.username':this.username,
-  'profile.service':'local'
-},(err, profile) => {
-  console.log('asd')
-  if(err) return cb(err)
-  return cb(null, profile)
 })
